d7f79bd2966a12f08708badea14ff85bf1342945
henryswain <henryswain3141@gmail.com>
2024-05-10 17:53:53 -0500

    update main second import statement

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 //import './assets/main.css'
 import './bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap'
+import './bootstrap'
 import 'papaparse/papaparse.js'
 
 import { createApp } from 'vue'
