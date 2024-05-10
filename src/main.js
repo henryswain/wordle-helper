d87afc9b249cd59747baadcaa5a6badd8b76549d
henryswain <henryswain3141@gmail.com>
2024-05-10 17:32:16 -0500

    back to first commit

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import "/node_modules/bootstrap"
-import "/node_modules/papaparse/papaparse.js"
-
-import { createApp } from '/node_modules/vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
