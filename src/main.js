94181b38e7f8f9c9395be9bfd7c728c79eeaef3e
henryswain <henryswain3141@gmail.com>
2024-05-10 14:46:53 -0500

    altered filepath in import statements

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap'
-import 'papaparse/papaparse.js'
+import 'node_modules/bootstrap/dist/css/bootstrap.min.css'
+import 'node_modules/bootstrap/dist/js/bootstrap'
+import 'node_modules/papaparse/papaparse.js'
 
 import { createApp } from 'vue'
 import App from './App.vue'
