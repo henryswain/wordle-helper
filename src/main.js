9ae179a155810b33f77b425e2425ba6a93ca4972
henryswain <henryswain3141@gmail.com>
2024-05-10 15:35:06 -0500

    slightly altered filepath again

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 //import 'node_modules/bootstrap/dist/css/bootstrap.min.css'
 //import 'node_modules/bootstrap/dist/js/bootstrap.js'
-import "/node_modules/bootstrap"
-import '/node_modules/papaparse/papaparse.js'
+import "node_modules/bootstrap"
+import 'node_modules/papaparse/papaparse.js'
 
 import { createApp } from 'vue'
 import App from './App.vue'
