902978bfd9bb64e94f243ffc7ba0e41d78ced1ea
henryswain <henryswain3141@gmail.com>
2024-05-10 15:49:26 -0500

    completed file path to vue

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import "/node_modules/bootstrap"
 import "/node_modules/papaparse/papaparse.js"
 
-import { createApp } from 'vue'
+import { createApp } from '/node_modules/vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
