da813bc5520cdd26f41c04612c3adb2a1e58fc1f
henryswain <henryswain3141@gmail.com>
2024-05-19 17:24:01 -0500

    changes script tag type of cdn link to importmap

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 //import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 //import 'papaparse/papaparse.js'
 
+//import { createApp } from 'vue'
 import { createApp } from 'vue'
 import App from './App.vue'
 
