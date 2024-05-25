9e4e43624d0b9409a666326001de480a68a03b4a
henryswain <henryswain3141@gmail.com>
2024-05-25 16:55:45 -0500

    fixed vue cdn link and commented out bootstrap cdn link

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
 
 
-//import { createApp } from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
